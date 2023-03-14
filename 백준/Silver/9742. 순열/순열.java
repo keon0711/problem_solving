8dad91d76e041e73faae4ff056af4ccc8844da8d
keon0711 <110909423+keon0711@users.noreply.github.com>
2023-03-14 17:32:50 +0900

    [Silver III] Title: 순열, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/9742. 순열/순열.java
+++ 백준/Silver/9742. 순열/순열.java
@@ -1,36 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    void solution() throws IOException {
+        int[] fact = new int[]{1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800};
 
-    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String str,ch;
-        int sz,l,i;
-        int[] len = {1,1,2,6,24,120,720,5040,40320,362880,3628880};
-        while((str=br.readLine())!=null) {
+        while (true) {
+            String str = br.readLine();
+            if (str == null) {
+                break;
+            }
             StringTokenizer st = new StringTokenizer(str);
-            ch = st.nextToken();
-            sz = ch.length();
-            l = Integer.parseInt(st.nextToken());
-            sb.append(ch+" "+l+" = ");
-            if(l>len[sz]) {
-                sb.append("No permutation\n");
-                continue;
+            String input = st.nextToken();
+            List<Character> li = new LinkedList<>();
+            for (Character character : input.toCharArray()) {
+                li.add(character);
             }
-            ArrayList<Character> q = new ArrayList<Character>();
-            for(i=0;i<sz;i++) q.add(ch.charAt(i));
-            l--;
-            for(i=sz-1;i>=0;) {
-                sb.append(q.remove(l/len[i]));
-                l %= len[i--];
+            int index = Integer.parseInt(st.nextToken());
+
+            System.out.printf("%s %d = ", input, index);
+            if (fact[input.length()] < index) {
+                System.out.println("No permutation");
+            } else {
+                StringBuilder sb = new StringBuilder();
+                index -= 1;
+                for (int i = input.length() - 1; i >= 0; i--) {
+                    sb.append(li.get(index / fact[i]));
+                    li.remove(index / fact[i]);
+                    index %= fact[i];
+                }
+                System.out.println(sb);
             }
-            sb.append("\n");
         }
-        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
     }
 }
