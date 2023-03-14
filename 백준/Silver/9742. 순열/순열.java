036c9b7cc0054b5a78420e7fb05b21f82bf38c1b
keon0711 <110909423+keon0711@users.noreply.github.com>
2023-03-14 17:21:33 +0900

    [Silver III] Title: 순열, Time: 156 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Silver/9742. 순열/순열.java
+++ 백준/Silver/9742. 순열/순열.java
@@ -5,32 +5,32 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String str,ch;
-		int sz,l,i;
-		int[] len = {1,1,2,6,24,120,720,5040,40320,362880,3628880};
-		while((str=br.readLine())!=null) {
-			StringTokenizer st = new StringTokenizer(str);
-			ch = st.nextToken();
-			sz = ch.length();
-			l = Integer.parseInt(st.nextToken());
-			sb.append(ch+" "+l+" = ");
-			if(l>len[sz]) {
-				sb.append("No permutation\n");
-				continue;
-			}
-	        ArrayList<Character> q = new ArrayList<Character>();
-	        for(i=0;i<sz;i++) q.add(ch.charAt(i));
-	        l--;
-	        for(i=sz-1;i>=0;) {
-	        	sb.append(q.remove(l/len[i]));
-	        	l %= len[i--];
-	        }
-	        sb.append("\n");
-		}
-		System.out.println(sb);
-	}
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str,ch;
+        int sz,l,i;
+        int[] len = {1,1,2,6,24,120,720,5040,40320,362880,3628880};
+        while((str=br.readLine())!=null) {
+            StringTokenizer st = new StringTokenizer(str);
+            ch = st.nextToken();
+            sz = ch.length();
+            l = Integer.parseInt(st.nextToken());
+            sb.append(ch+" "+l+" = ");
+            if(l>len[sz]) {
+                sb.append("No permutation\n");
+                continue;
+            }
+            ArrayList<Character> q = new ArrayList<Character>();
+            for(i=0;i<sz;i++) q.add(ch.charAt(i));
+            l--;
+            for(i=sz-1;i>=0;) {
+                sb.append(q.remove(l/len[i]));
+                l %= len[i--];
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
