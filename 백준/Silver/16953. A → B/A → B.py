d61034ea8a2ce34c52ba31622d0e84133c4f0876
Keon Kim <110909423+keon0711@users.noreply.github.com>
2023-04-25 01:38:52 +0900

    [Silver II] Title: A → B, Time: 64 ms, Memory: 34120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -7,16 +7,18 @@ sys.setrecursionlimit(100000)
 def solution():
     A, B = map(int, input().split())
     Q = deque()
-    Q.append((A, 1))
+    Q.append((B, 1))
     while Q:
         cur_val, cur_cnt = Q.popleft()
-        if cur_val == B:
+        if cur_val == A:
             print(cur_cnt)
             return
-        if cur_val > B:
+        if cur_val < A:
             continue
-        Q.append((cur_val * 2, cur_cnt + 1))
-        Q.append((cur_val * 10 + 1, cur_cnt + 1))
+        if cur_val % 2 == 0:
+            Q.append((cur_val // 2, cur_cnt + 1))
+        if cur_val % 10 == 1:
+            Q.append((cur_val // 10, cur_cnt + 1))
 
     print(-1)
     return
