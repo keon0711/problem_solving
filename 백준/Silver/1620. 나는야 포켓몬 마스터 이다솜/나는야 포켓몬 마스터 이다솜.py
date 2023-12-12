380aa5638c24c9df20bdb71df53f5dc7d4bdecd8
Keon Kim <110909423+keon0711@users.noreply.github.com>
2023-12-12 15:23:52 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 280 ms, Memory: 130380 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,16 +1,17 @@
-from collections import defaultdict
+import sys
 
+input = sys.stdin.readline
 N, M = map(int, input().split())
-DICT = defaultdict(int)
+DICT = dict()
 LIST = [""] * (N + 1)
 
 for i in range(1, N + 1):
-    name = input()
+    name = input().rstrip()
     DICT[name] = i
     LIST[i] = name
 
 for _ in range(M):
-    question = input()
+    question = input().rstrip()
     if question.isnumeric():
         print(LIST[int(question)])
     else:
