2830632adaaf0b13366dd028258d0a002c889fe0
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-01-31 11:43:26 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char c = sc.nextLine().charAt(0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
         System.out.println((int) c);
     }
 }
