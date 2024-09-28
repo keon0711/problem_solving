b7f3c486737a0d150928e0b43b040758c6763e64
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-09-29 06:17:04 +0900

    [level 2] Title: 리코쳇 로봇, Time: 4.25 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,63 +1,72 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
+    static int n, m, sx, sy, ex, ey;
+    static char[][] map;
+    static boolean[][] visit;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-    private final int[] dx = new int[]{1, -1, 0, 0};
-    private final int[] dy = new int[]{0, 0, 1, -1};
+    public static int bfs(){
+        Queue<int[]> q = new LinkedList<int[]>();
+        visit[sx][sy] = true;
+        q.add(new int[]{sx, sy, 0});
 
-    public int solution(String[] board) {
-        char[][] map = Arrays.stream(board)
-            .map(String::toCharArray)
-            .toArray(char[][]::new);
+        while(!q.isEmpty()){
+            int[] point = q.poll();
 
-        Queue<int[]> pq = new LinkedList<>();
+            int x = point[0];
+            int y = point[1];
+            int dist = point[2];
 
-        for (int row = 0; row < board.length; row++) {
-            int col = board[row].indexOf("R");
-            if (col != -1) {
-                pq.add(new int[]{row, col, 0});
-                map[row][col] = 'x';
-                break;
-            }
-        }
+            if(x==ex && y==ey) return dist;
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int row = cur[0];
-            int col = cur[1];
-            int moveCount = cur[2];
+            for(int i=0;i<4;i++){
+                int nextX = x;
+                int nextY = y;
 
-            for (int i = 0; i < 4; i++) {
-                int nextRow = row;
-                int nextCol = col;
-                while (isMovable(nextRow, nextCol, i, map)) {
-                    nextRow += dx[i];
-                    nextCol += dy[i];
+                for(int k=1;k<=Math.max(n, m);k++){
+                    nextX+=dx[i];
+                    nextY+=dy[i];
+
+                    if(nextX<0 || nextX>=n || nextY<0 || nextY>=m || map[nextX][nextY] =='D') {
+                        nextX-=dx[i];
+                        nextY-=dy[i];
+                        break;
+                    }
                 }
 
-                if (map[nextRow][nextCol] == 'G') {
-                    return moveCount + 1;
-                }
+                if(visit[nextX][nextY]) continue;
 
-                if (map[nextRow][nextCol] != 'x') {
-                    pq.add(new int[]{nextRow, nextCol, moveCount + 1});
-                    map[nextRow][nextCol] = 'x';
-                }
+                visit[nextX][nextY] = true;
+                q.add(new int[]{nextX, nextY, dist+1});
             }
         }
 
         return -1;
-
     }
 
-    private boolean isMovable(int row, int col, int i, char[][] map) {
-        int nextRow = row + dx[i];
-        int nextCol = col + dy[i];
-        return nextRow >= 0 && nextRow < map.length && nextCol >= 0
-            && col + dy[i] < map[0].length && map[nextRow][nextCol] != 'D';
+    public int solution(String[] board) {
+        n = board.length;
+        m = board[0].length();
+
+        map = new char[n][m];
+        visit = new boolean[n][m];
+
+        for(int i=0;i<n;i++) {
+            map[i] = board[i].toCharArray();
+            for(int j=0;j<m;j++) {
+                if(map[i][j] == 'R'){
+                    sx = i;
+                    sy = j;
+                }
+                else if(map[i][j] == 'G'){
+                    ex = i;
+                    ey = j;
+                }
+            }    
+        }
+
+        return bfs();
     }
 }
