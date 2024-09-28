64f20774b2201cb8bb798e2f74c2c1fe085a917a
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-09-29 06:12:38 +0900

    [level 2] Title: 리코쳇 로봇, Time: 33.74 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+
+    private final int[] dx = new int[]{1, -1, 0, 0};
+    private final int[] dy = new int[]{0, 0, 1, -1};
+
+    public int solution(String[] board) {
+        char[][] map = Arrays.stream(board)
+            .map(String::toCharArray)
+            .toArray(char[][]::new);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[2]));
+
+        for (int row = 0; row < board.length; row++) {
+            int col = board[row].indexOf("R");
+            if (col != -1) {
+                pq.add(new int[]{row, col, 0});
+                map[row][col] = 'x';
+                break;
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int row = cur[0];
+            int col = cur[1];
+            int moveCount = cur[2];
+
+            System.out.println(row + "," + col + "," + moveCount);
+
+
+            for (int i = 0; i < 4; i++) {
+                int nextRow = row;
+                int nextCol = col;
+                while (isMovable(nextRow, nextCol, i, map)) {
+                    nextRow += dx[i];
+                    nextCol += dy[i];
+                }
+                
+                if (map[nextRow][nextCol] == 'G') {
+                    return moveCount + 1;
+                }
+
+                if (map[nextRow][nextCol] != 'x') {
+                    pq.add(new int[]{nextRow, nextCol, moveCount + 1});
+                    map[nextRow][nextCol] = 'x';
+                }
+            }
+        }
+
+        return -1;
+
+    }
+
+    private boolean isMovable(int row, int col, int i, char[][] map) {
+        int nextRow = row + dx[i];
+        int nextCol = col + dy[i];
+        return nextRow >= 0 && nextRow < map.length && nextCol >= 0
+            && col + dy[i] < map[0].length && map[nextRow][nextCol] != 'D';
+    }
+}
