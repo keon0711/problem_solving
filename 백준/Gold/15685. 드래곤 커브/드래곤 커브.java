4c6c8763fa7b7d1b9b34d119a50109fab05a2d80
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-10-17 23:18:02 +0900

    [Gold III] Title: 드래곤 커브, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
@@ -1,59 +1,62 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static boolean[][] grid = new boolean[101][101];
+    static int[] dx = {1, 0, -1, 0}; // 0: 오른쪽, 1: 위, 2: 왼쪽, 3: 아래
+    static int[] dy = {0, -1, 0, 1};
 
-    static int[] dx = new int[]{1, 0, -1, 0};
-    static int[] dy = new int[]{0, -1, 0, 1};
-    static int[][] map = new int[101][101];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int d = sc.nextInt();
-            int g = sc.nextInt();
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()); // 시작 x 좌표
+            int y = Integer.parseInt(st.nextToken()); // 시작 y 좌표
+            int d = Integer.parseInt(st.nextToken()); // 시작 방향
+            int g = Integer.parseInt(st.nextToken()); // 세대
 
-            dragonCurve(x, y, d, g);
-        }
+            List<Integer> directions = new ArrayList<>();
+            directions.add(d);
+            generateDirections(directions, g);
 
-        System.out.println(countSquare());
-    }
-
-    private static int countSquare() {
-        int squareCount = 0;
-
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (map[i][j] == 1 && map[i + 1][j] == 1 && map[i][j + 1] == 1 && map[i + 1][j + 1] == 1) {
-                    squareCount++;
+            grid[x][y] = true;
+            for(int dir : directions){
+                x += dx[dir];
+                y += dy[dir];
+                if(x >= 0 && x <= 100 && y >= 0 && y <= 100){
+                    grid[x][y] = true;
                 }
             }
         }
 
-        return squareCount;
-    }
-
-    private static void dragonCurve(int x, int y, int d, int g) {
-        List<Integer> directions = new ArrayList<>();
-        directions.add(d);
-
-        map[y][x] = 1;
-        x += dx[d];
-        y += dy[d];
-        map[y][x] = 1;
-
-        for (int i = 0; i < g; i++) {
-            int size = directions.size();
-            for (int j = size - 1; j >= 0; j--) {
-                d = (directions.get(j) + 1) % 4;
-                x += dx[d];
-                y += dy[d];
-                map[y][x] = 1;
-                directions.add(d);
+        // 네 꼭짓점이 모두 드래곤 커브의 일부인 정사각형 개수 세기
+        int count = 0;
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j < 100; j++){
+                if(grid[i][j] && grid[i+1][j] && grid[i][j+1] && grid[i+1][j+1]){
+                    count++;
+                }
             }
         }
+
+        System.out.println(count);
+    }
+
+    // 재귀적으로 방향 정보를 생성하는 함수
+    public static void generateDirections(List<Integer> directions, int generation){
+        if(generation == 0){
+            return;
+        }
+
+        int size = directions.size();
+        for(int i = size - 1; i >= 0; i--){
+            int dir = (directions.get(i) + 1) % 4;
+            directions.add(dir);
+        }
+
+        generateDirections(directions, generation - 1);
     }
 }
