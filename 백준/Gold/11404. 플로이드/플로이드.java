05ff610be972010efaa912af28a1712b0b3f2fb7
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-10-25 21:12:29 +0900

    [Gold IV] Title: 플로이드, Time: 1124 ms, Memory: 249408 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,49 @@
+import static java.lang.Integer.MAX_VALUE;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    static final int INF = 100_000_000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        long[][] dist = new long[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dist[i][j] = (i == j) ? 0 : INF;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            int cost = sc.nextInt();
+
+            dist[from][to] = Math.min(cost, dist[from][to]);
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
