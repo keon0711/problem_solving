e11018bff6e0346a697aeed5348c1fe97e231dc5
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-11-24 13:12:36 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 196 ms, Memory: 18092 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -4,32 +4,38 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner sc;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         int T = sc.nextInt();
 
         while (T-- > 0) {
-            int N = sc.nextInt();
-            if (N == 0) {
-                System.out.println(0);
-                continue;
-            }
-
-            Map<String, Integer> counter = new HashMap<>();
-            for (int i = 0; i < N; i++) {
-                String clothes = sc.next(); // 의상의 이름 (사용하지 않음)
-                String type = sc.next();    // 의상의 종류
-                counter.merge(type, 1, Integer::sum);
-            }
-
-            int result = 1;
-            for (int count : counter.values()) {
-                result *= (count + 1);
-            }
-
-            System.out.println(result - 1);
+            simulate();
         }
 
         sc.close();
+    }
+
+    private static void simulate() {
+        int N = sc.nextInt();
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        Map<String, Integer> counter = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            String clothes = sc.next(); // 의상의 이름 (사용하지 않음)
+            String type = sc.next();    // 의상의 종류
+            counter.merge(type, 1, Integer::sum);
+        }
+
+        int result = 1;
+        for (int count : counter.values()) {
+            result *= (count + 1);
+        }
+
+        System.out.println(result - 1);
     }
 }
