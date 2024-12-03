3c4b8ffa8ccc6a8eb28a9514b96579d1aaf3a1e3
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-12-03 16:22:40 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 472 ms, Memory: 48556 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
@@ -1,95 +1,82 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] parent, candy, groupSize;
+    static boolean[] visited;
+    static int N, M, K;
 
-    private static boolean[] visited;
-    private static List<Group> groups;
-    private static int[] candies;
-    private static Map<Integer, List<Integer>> graph;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int K = sc.nextInt();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        candies = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            candies[i] = sc.nextInt();
-        }
-
-        graph = new HashMap<>();
-        for (int i = 0; i < M; i++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-
-            graph.computeIfAbsent(A, k -> new ArrayList<>()).add(B);
-            graph.computeIfAbsent(B, k -> new ArrayList<>()).add(A);
-        }
-
-        List<Group> groups = new ArrayList<>();
+        parent = new int[N + 1];
+        candy = new int[N + 1];
+        groupSize = new int[N + 1];
         visited = new boolean[N + 1];
+
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                groups.add(dfs(i));
+            parent[i] = i;
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            candy[i] = Integer.parseInt(st.nextToken());
+            groupSize[i] = 1;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            union(a, b);
+        }
+
+        Map<Integer, Integer> groupCandy = new HashMap<>();
+        Map<Integer, Integer> groupCount = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            int root = find(i);
+            groupCandy.put(root, groupCandy.getOrDefault(root, 0) + candy[i]);
+            groupCount.put(root, groupCount.getOrDefault(root, 0) + 1);
+        }
+
+        List<int[]> groups = new ArrayList<>();
+        for (int key : groupCandy.keySet()) {
+            groups.add(new int[]{groupCount.get(key), groupCandy.get(key)});
+        }
+
+        int[] dp = new int[K];
+        for (int[] group : groups) {
+            int size = group[0];
+            int candies = group[1];
+            for (int j = K - 1; j >= size; j--) {
+                dp[j] = Math.max(dp[j], dp[j - size] + candies);
             }
         }
 
-        int[][] dp = new int[groups.size() + 1][K + 1];
-
-        for (int i = 1; i <= groups.size(); i++) {
-            for (int j = 0; j <= K; j++) {
-                Group group = groups.get(i - 1);
-
-                if (group.people <= j) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - group.people] + group.candy);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
+        int maxCandies = 0;
+        for (int i = 0; i < K; i++) {
+            maxCandies = Math.max(maxCandies, dp[i]);
         }
 
-        System.out.println(dp[groups.size()][K - 1]);
-
+        System.out.println(maxCandies);
     }
 
-    private static Group dfs(int i) {
-        visited[i] = true;
-
-        List<Integer> friends = graph.getOrDefault(i, Collections.emptyList());
-        Group group = new Group(candies[i], 1);
-
-        for (int friend : friends) {
-            if (!visited[friend]) {
-                group.add(dfs(friend));
-            }
-        }
-
-        return group;
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
     }
 
-    static class Group {
-
-        int candy;
-        int people;
-
-        public Group(int candy, int people) {
-            this.candy = candy;
-            this.people = people;
-        }
-
-        public void add(Group group) {
-            this.candy += group.candy;
-            this.people += group.people;
-        }
-
-        @Override
-        public String toString() {
-            return "Group{" +
-                "candy=" + candy +
-                ", people=" + people +
-                '}';
+    static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
         }
     }
-
 }
