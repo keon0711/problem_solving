4b58f88a8c05a03367cc01eee054139929400e1c
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-12-07 23:18:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.58 ms, Memory: 94.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String solution(int[] food) {
+        StringBuilder leftSide = new StringBuilder();
+
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            for (int j = 0; j < count; j++) {
+                leftSide.append(i);
+            }
+        }
+
+
+        return leftSide.toString() + "0" + leftSide.reverse().toString();
+    }
+}
